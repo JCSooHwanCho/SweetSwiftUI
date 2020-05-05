5ec2a329236eb2aabfcd594e3d7d43d1ae29469f
SoohwanCho <chh0107@gmail.com>
2020-05-05 17:12:26 +0900

    2. 기본 뷰 구성하기 까지

--- FruitMart/Source/User Interface/Home/Home.swift
+++ FruitMart/Source/User Interface/Home/Home.swift
@@ -9,13 +9,50 @@
 import SwiftUI
 
 struct Home: View {
-  var body: some View {
-    Text("Hello, World!")
-  }
+    var body: some View {
+        HStack {
+            Image("apple")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 140)
+                .clipped()
+
+            VStack {
+                Text("백설공주 사과")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .padding(.bottom, 6)
+
+                Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+
+                Spacer()
+                HStack(spacing: 0) {
+                    Text("2100").font(.headline) + Text("원").font(.footnote)
+
+                    Spacer()
+
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                        .foregroundColor(Color("peach"))
+                        .frame(width: 32, height: 32)
+                    Image(systemName: "cart")
+                        .foregroundColor(Color("peach"))
+                        .frame(width: 32, height: 32)
+                }
+            }
+        }
+        .frame(height: 150)
+        .background(Color.primary.colorInvert())
+        .cornerRadius(6)
+        .shadow(color: Color.primary.opacity(0.33), radius: 1, x: 2, y: 2)
+        .padding(.vertical, 8)
+    }
 }
 
 struct Home_Previews: PreviewProvider {
-  static var previews: some View {
-    Home()
-  }
+    static var previews: some View {
+        Home()
+    }
 }
