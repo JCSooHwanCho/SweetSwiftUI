ca2dc974f4023b4b2a99b8819ad00b38c1cd69f4
SoohwanCho <chh0107@gmail.com>
2020-05-09 23:11:55 +0900

    4. 프로퍼티로 뷰 추출하기

--- FruitMart/Source/User Interface/Home/ProductRow.swift
+++ FruitMart/Source/User Interface/Home/ProductRow.swift
@@ -11,37 +11,8 @@ import SwiftUI
 struct ProductRow: View {
     var body: some View {
         HStack {
-            Image("apple")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 140)
-                .clipped()
-
-            VStack {
-                Text("백설공주 사과")
-                    .font(.headline)
-                    .fontWeight(.medium)
-                    .padding(.bottom, 6)
-
-                Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
-                    .font(.footnote)
-                    .foregroundColor(.secondary)
-
-                Spacer()
-                HStack(spacing: 0) {
-                    Text("2100").font(.headline) + Text("원").font(.footnote)
-
-                    Spacer()
-
-                    Image(systemName: "heart")
-                        .imageScale(.large)
-                        .foregroundColor(Color("peach"))
-                        .frame(width: 32, height: 32)
-                    Image(systemName: "cart")
-                        .foregroundColor(Color("peach"))
-                        .frame(width: 32, height: 32)
-                }
-            }
+            productImage
+            productDescription
         }
         .frame(height: 150)
         .background(Color.primary.colorInvert())
@@ -55,5 +26,48 @@ struct ProductRow: View {
 struct ProductRow_Previews: PreviewProvider {
     static var previews: some View {
         ProductRow()
+    }
+}
+
+extension ProductRow {
+    var productImage: some View {
+        Image("apple")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 140)
+            .clipped()
+    }
+
+    var productDescription: some View {
+        VStack {
+            Text("백설공주 사과")
+                .font(.headline)
+                .fontWeight(.medium)
+                .padding(.bottom, 6)
+
+            Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+
+            Spacer()
+
+            footerView
+        }
+    }
+
+    var footerView: some View {
+        HStack(spacing: 0) {
+            Text("2100").font(.headline) + Text("원").font(.footnote)
+
+            Spacer()
+
+            Image(systemName: "heart")
+                .imageScale(.large)
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32)
+            Image(systemName: "cart")
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32)
+        }
     }
 }
