f012e045163a77078ec4e47f215133436b0c14d8
SooHwanCho <chh0107@gmail.com>
2020-06-28 21:30:43 +0900

    32. 사용자 정보 나타내기 까지

--- FruitMart/Source/User Interface/MyPage/MyPage.swift
+++ FruitMart/Source/User Interface/MyPage/MyPage.swift
@@ -10,15 +10,22 @@ import SwiftUI
 
 struct MyPage: View {
     @EnvironmentObject var store: Store
+    @State private var pickedImage: Image = Image(systemName: "person.crop.circle")
+    @State private var nickname = ""
+
     private let pickerDataSource: [CGFloat] = [140, 150, 160]
 
     var body: some View {
         NavigationView {
-            Form {
-                orderInfoSelection
-                appSettingSection
+            VStack {
+                userInfo
+                nicknameTextField
+                Form {
+                    orderInfoSelection
+                    appSettingSection
+                }
+                .navigationBarTitle("마이페이지")
             }
-            .navigationBarTitle("마이페이지")
         }
     }
 
@@ -50,6 +57,39 @@ struct MyPage: View {
             }
             .pickerStyle(SegmentedPickerStyle())
         }.frame(height: 72)
+    }
+
+    var userInfo: some View {
+        VStack {
+            profileImage
+        }.frame(maxWidth: .infinity, minHeight: 200)
+        .background(Color.background)
+    }
+
+    var profileImage: some View {
+        pickedImage
+            .resizable().scaledToFill()
+            .clipShape(Circle())
+            .frame(width: 100, height: 100)
+            .overlay(pickImageButton.offset(x:8, y:0), alignment: .bottomTrailing)
+    }
+
+    var pickImageButton: some View {
+        Button(action: {}) {
+            Circle()
+                .fill(Color.white)
+                .frame(width: 32, height: 32)
+                .shadow(color: .primaryShadow, radius: 2, x: 2, y: 2)
+                .overlay(Image("pencil").foregroundColor(.black))
+        }
+    }
+
+    var nicknameTextField: some View {
+        TextField("닉네임", text: $nickname)
+            .font(.system(size: 25, weight: .medium))
+            .textContentType(.nickname)
+            .multilineTextAlignment(.center)
+            .autocapitalization(.none)
     }
 }
 
